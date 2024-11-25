7462e387a88e69562f6331e9130125e8c12d10f2
Leanne Boulton <leanne.elizabeth@gmail.com>
2024-11-25 17:58:04 -0500

    Adjusted code in Rectangle Solver so fix but with sort function

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -88,83 +88,75 @@ double AreaOfRectangle(int x1, int y1, int x2, int y2, int x3, int y3) {
 
 //main rectangle function
 bool analyzeRectangle(CORNERS* points) {
-	*points = sortPoints(points);	//TODO: fix point storage from sort points
+	CORNERS* sortedPoints = sortPoints(points);  // Get sorted points
 
-	int perimeter = PerimeterFrom4Points(points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y, points[3].x, points[3].y);
-	
-	//char result = ("IsRectangle = False\nPerimeter of shape = %d", perimeter);
+	// Calculate the perimeter using the sorted points
+	int perimeter = PerimeterFrom4Points(
+		sortedPoints[0].x, sortedPoints[0].y,
+		sortedPoints[1].x, sortedPoints[1].y,
+		sortedPoints[2].x, sortedPoints[2].y,
+		sortedPoints[3].x, sortedPoints[3].y
+	);
 
-	if (CheckRightAngle(points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y, points[3].x, points[3].y)) {
-		float area = AreaOfRectangle(points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y);
+	// Check right angles using the sorted points
+	if (CheckRightAngle(
+		sortedPoints[0].x, sortedPoints[0].y,
+		sortedPoints[1].x, sortedPoints[1].y,
+		sortedPoints[2].x, sortedPoints[2].y,
+		sortedPoints[3].x, sortedPoints[3].y
+	)) {
+
+		// Calculate the area using the sorted points
+		float area = AreaOfRectangle(
+			sortedPoints[0].x, sortedPoints[0].y,
+			sortedPoints[1].x, sortedPoints[1].y,
+			sortedPoints[2].x, sortedPoints[2].y
+		);
+
 		printf("IsRectangle = True\nPerimeter = %d\nRectangle Area = %f", perimeter, area);
 		return true;
 	}
+
 	printf("IsRectangle = False\nPerimeter = %d\n", perimeter);
 	return false;
 }
 
-// sorts points given by user to desired format
-CORNERS sortPoints(CORNERS* points) {
-	//given: 3,3  4,5  5,3  2,5  
-	//should recieve: 3,3  5,3  4,5  2,5
+CORNERS* sortPoints(CORNERS* points) {
+	// Arrays to hold the x and y coordinates.
 	int tx1 = points[0].x; int tx2 = points[1].x; int tx3 = points[2].x; int tx4 = points[3].x;
 	int arrayX[] = { tx1, tx2, tx3, tx4 };
 	int ty1 = points[0].y; int ty2 = points[1].y; int ty3 = points[2].y; int ty4 = points[3].y;
 	int arrayY[] = { ty1, ty2, ty3, ty4 };
-	// find center point for x's any y's by adding them together then dividing by 4
+
+	// Find the center point for sorting.
 	float sumX = (tx1 + tx2 + tx3 + tx4);
 	float centerX = (sumX / POINTS);
 	float sumY = (ty1 + ty2 + ty3 + ty4);
 	float centerY = (sumY / POINTS);
 
-
-	// if x > centerX then it is in quadrant 2 or 3 (assign as outer value) 2
-	// if x < centerX then it is in quadrant 1 or 4 (assign as inner value) 1
-	int quadX[] = { 0, 0, 0, 0 };
+	// Determine the quadrant of each point based on the center point.
+	int quadX[POINTS] = { 0 };
+	int quadY[POINTS] = { 0 };
 	for (int i = 0; i < POINTS; i++) {
-		if (arrayX[i] > centerX) {
-			quadX[i] = UPPER;
-		}
-		else {
-			quadX[i] = LOWER;
-		}
+		quadX[i] = (arrayX[i] > centerX) ? UPPER : LOWER;
+		quadY[i] = (arrayY[i] > centerY) ? UPPER : LOWER;
 	}
 
-	// if y > centerY then it is in quadrant 3 or 4 (assign as upper value) 2
-	// if y < centerY then it is in quadrant 1 or 2 (assign as lower value) 1
-	int quadY[] = { 0, 0, 0, 0 };
+	// Sort the points into a new array based on their quadrant values.
+	static CORNERS newPoints[POINTS];
 	for (int i = 0; i < POINTS; i++) {
-		if (arrayY[i] > centerY) {
-			quadY[i] = UPPER;
+		if (quadX[i] == LOWER && quadY[i] == LOWER) {
+			newPoints[0] = points[i];
 		}
-		else {
-			quadY[i] = LOWER;
+		else if (quadX[i] == UPPER && quadY[i] == LOWER) {
+			newPoints[1] = points[i];
+		}
+		else if (quadX[i] == UPPER && quadY[i] == UPPER) {
+			newPoints[2] = points[i];
+		}
+		else if (quadX[i] == LOWER && quadY[i] == UPPER) {
+			newPoints[3] = points[i];
 		}
 	}
-
-	CORNERS newPoints[POINTS];
-	//	sorting the points using the quadrants
-	for (int i = 0; i < POINTS; i++) {
-		// p1 = lower && lower
-		if ((quadX[i] == LOWER) && (quadY[i] == LOWER)) {
-			newPoints[0].x = arrayX[i];
-			newPoints[0].y = arrayY[i];
-		}
-		// p2 = upperX && lowerY
-		if ((quadX[i] == UPPER) && (quadY[i] == LOWER)) {
-			newPoints[1].x = arrayX[i];
-			newPoints[1].y = arrayY[i];
-		}
-		// p3 = upper && upper
-		if ((quadX[i] == UPPER) && (quadY[i] == UPPER)) {
-			newPoints[2].x = arrayX[i];
-			newPoints[2].y = arrayY[i];
-		}
-		// p4 = lowerX && upperY
-		if ((quadX[i] == LOWER) && (quadY[i] == UPPER)) {
-			newPoints[3].x = arrayX[i];
-			newPoints[3].y = arrayY[i];
-		}
-	}
-	return *newPoints;
+	return newPoints;
 }
