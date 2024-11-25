03f604ecfcfb1f2480582dace12739fd7b418649
Leanne Boulton <leanne.elizabeth@gmail.com>
2024-11-25 13:13:51 -0500

    created Tests for Triangle Solver

--- UnitTest_RectangleSolver/UnitTest_RectangleSolver.cpp
+++ UnitTest_RectangleSolver/UnitTest_RectangleSolver.cpp
@@ -1,9 +1,17 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-typedef struct corners {
+
+
+typedef struct{
 	int x;
 	int y;
-} CORNERS, PTCORNERS; 
+} CORNERS; 
+
+extern "C" char* analyzeRectangle(CORNERS points[]);
+extern "C" double PerimeterFrom4Points(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4);
+extern "C" int ValidateInput(CORNERS points);
+extern "C" double AreaOfRectangle(int x1, int y1, int x2, int y2, int x3, int y3);
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
